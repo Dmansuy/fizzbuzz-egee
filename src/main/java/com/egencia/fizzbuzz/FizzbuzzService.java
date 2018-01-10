b09b3411df63ac426c8e51f687dbdf528fa64ce1
dmansuy <t-dmansuy@egencia.com>
2018-01-10 16:03:54 +0100

    initial commit

---
+++ src/main/java/com/egencia/fizzbuzz/FizzbuzzService.java
@@ -0,0 +1,18 @@
+package com.egencia.fizzbuzz;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FizzbuzzService {
+    public List<Integer> generate(int myNumber) {
+
+
+        return Collections.emptyList();
+    }
+
+    public static void main(String[] args) {
+        FizzbuzzService fizzbuzzService = new FizzbuzzService();
+
+        System.out.println(fizzbuzzService.generate(2));
+    }
+}
