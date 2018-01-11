9887fd282002e4f938218982f0ee0e3476dc2a2b
dmansuy <t-dmansuy@egencia.com>
2018-01-11 11:37:58 +0100

    FizzBuzz first version

--- src/main/java/com/egencia/fizzbuzz/FizzbuzzService.java
+++ src/main/java/com/egencia/fizzbuzz/FizzbuzzService.java
@@ -6,13 +6,29 @@ import java.util.List;
 public class FizzbuzzService {
     public List<Integer> generate(int myNumber) {
 
+        for (int i = 1; i < myNumber; i++) {
 
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(" FizzBuzz ");
+            } else {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    if (i % 3 == 0) {
+                        System.out.print(" Fizz ");
+                    }
+                    if (i % 5 == 0) {
+                        System.out.print(" Buzz ");
+                    }
+                } else {
+                    System.out.print(" " + i + " ");
+                }
+            }
+        }
         return Collections.emptyList();
     }
 
     public static void main(String[] args) {
         FizzbuzzService fizzbuzzService = new FizzbuzzService();
 
-        System.out.println(fizzbuzzService.generate(2));
+        System.out.println(fizzbuzzService.generate(17));
     }
 }
